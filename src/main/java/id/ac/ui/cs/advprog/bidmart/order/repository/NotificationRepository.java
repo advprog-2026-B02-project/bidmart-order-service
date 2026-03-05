caae6ed5d195f998b600564321ee853243834cc2
newjeasnz <jesiccatandra@gmail.com>
2026-03-06 00:27:20 +0700

    feat: create booking & notification repository

---
+++ src/main/java/id/ac/ui/cs/advprog/bidmart/order/repository/NotificationRepository.java
@@ -0,0 +1,24 @@
+package id.ac.ui.cs.advprog.bidmart.order.repository;
+
+import id.ac.ui.cs.advprog.bidmart.order.model.Notification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, UUID> {
+    Page<Notification> findUserById(UUID userId, Pageable pageable);
+
+    Page<Notification> findByUserIdAndIsRead(UUID userId, Boolean isRead, Pageable pageable);
+
+    long countByUserIdAndIsRead(UUID userId, Boolean isRead);
+
+    @Modifying
+    @Query("UPDATE Notification n SET n.isRead = true, n.readAt = CURRENT_TIMESTAMP WHERE n.userId = :userId AND n.isRead = false")
+    void markAllAsReadByUserId(UUID userId);
+}
