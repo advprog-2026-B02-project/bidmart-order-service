1a6b513aae831553874d51d91074de56efc5903f
newjeasnz <jesiccatandra@gmail.com>
2026-04-14 14:08:25 +0700

    chore: remove notification module logic from order branch

--- src/main/java/id/ac/ui/cs/advprog/bidmart/order/controller/OrderController.java
+++ src/main/java/id/ac/ui/cs/advprog/bidmart/order/controller/OrderController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/orders")
+@RequestMapping("/orders")
 public class OrderController {
 
     private final OrderService orderService;
