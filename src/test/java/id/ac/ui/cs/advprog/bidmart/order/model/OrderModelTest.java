44a5a0e49a92c70820850698d1f49a5c1d0c83e0
newjeasnz <jesiccatandra@gmail.com>
2026-04-14 19:20:33 +0700

    refactor: move notification module to its own package

--- src/test/java/id/ac/ui/cs/advprog/bidmart/order/model/OrderModelTest.java
+++ src/test/java/id/ac/ui/cs/advprog/bidmart/order/model/OrderModelTest.java
@@ -1,8 +1,9 @@
 package id.ac.ui.cs.advprog.bidmart.order.model;
 
+import id.ac.ui.cs.advprog.bidmart.notifications.model.Notification;
+import id.ac.ui.cs.advprog.bidmart.notifications.model.NotificationType;
 import org.junit.jupiter.api.Test;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
