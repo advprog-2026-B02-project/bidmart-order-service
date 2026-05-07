784ede4ecb4577b08af2728b9b9187368b854b6d
newjeasnz <jesiccatandra@gmail.com>
2026-05-07 10:14:20 +0700

    fix test order

--- src/test/java/id/ac/ui/cs/advprog/bidmart/order/service/OrderServiceImplTest.java
+++ src/test/java/id/ac/ui/cs/advprog/bidmart/order/service/OrderServiceImplTest.java
@@ -1,7 +1,6 @@
 package id.ac.ui.cs.advprog.bidmart.order.service;
 
 import id.ac.ui.cs.advprog.bidmart.order.dto.SaveNotification;
-import id.ac.ui.cs.advprog.bidmart.order.service.NotificationService;
 import id.ac.ui.cs.advprog.bidmart.order.dto.CreateOrder;
 import id.ac.ui.cs.advprog.bidmart.order.dto.OrderListResponse;
 import id.ac.ui.cs.advprog.bidmart.order.dto.OrderResponse;
